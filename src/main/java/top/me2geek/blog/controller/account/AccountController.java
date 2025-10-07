acff303da20c0ebe41bbc740fa7a6a7cb1ce4a47
狼牙 <langya466@gmail.com>
2025-10-07 18:09:58 +0800

    refactor(core): 移除UserService并优化登录逻辑

--- src/main/java/top/me2geek/blog/controller/account/AccountController.java
+++ src/main/java/top/me2geek/blog/controller/account/AccountController.java
@@ -3,7 +3,8 @@ package top.me2geek.blog.controller.account;
 import org.springframework.web.bind.annotation.*;
 import top.me2geek.blog.common.ApiResponse;
 import top.me2geek.blog.data.LoginForm;
-import top.me2geek.blog.service.UserService;
+
+import java.util.UUID;
 
 /**
  * @author LangYa466
@@ -12,22 +13,33 @@ import top.me2geek.blog.service.UserService;
 @RestController
 @RequestMapping("/account")
 public class AccountController {
-    public static LoginForm adminAccount =  new LoginForm("admin", "admin", "adminToken");
-    private final UserService userService;
-
-    public AccountController(UserService userService) {
-        this.userService = userService;
-    }
+    public static LoginForm adminAccount = new LoginForm("admin", "admin", "adminToken");
 
     @PostMapping("/login")
     public ApiResponse<String> onLogin(@RequestBody LoginForm loginForm) {
         if (loginForm.toString().length() > 40) {
             return ApiResponse.fail("用户名或密码过长");
         }
-        String[] result = userService.login(loginForm);
-        if (result[0].equals("success")) {
-            return ApiResponse.success("登录成功", result[1]);
+        String token = handleLogin(loginForm);
+        if (token != null) {
+            return ApiResponse.success("登录成功", token);
         }
         return ApiResponse.fail("错误的用户名或密码");
+    }
+
+    private String handleLogin(LoginForm loginForm) {
+        if (adminAccount == null) return null;
+
+        if (loginForm.getUsername().equals(adminAccount.getUsername()) &&
+                loginForm.getPassword().equals(adminAccount.getPassword())) {
+            adminAccount.setToken(generateAPIToken());
+            return adminAccount.getToken();
+        }
+
+        return null;
+    }
+
+    private String generateAPIToken() {
+        return (UUID.randomUUID().toString() + UUID.randomUUID()).replace("-", "");
     }
 }
